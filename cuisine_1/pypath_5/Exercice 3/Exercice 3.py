1955264cd1b021b807fd3b0015e20c2a4292f6ff
Chanelle Kapawou <chanellekapawou@gmail.com>
2021-06-10 12:03:33 +0100

    Exexcice 3 terminé

--- cuisine_1/pypath_5/Exercice 3/Exercice 3.py
+++ cuisine_1/pypath_5/Exercice 3/Exercice 3.py
@@ -9,28 +9,34 @@ score_egal = 0       # nombre d'égalité
 
 # demande à l'utilisateur s'il veut jouer: y pour yes et n pour no
 
-while  (input("désirez vous Jouer? (y/n): ").lower()) != "n":                                    # tant qu'il sa reponse est oui
-    nbre_coup += 1
+while  (input("désirez vous Jouer? (cliquez sur une touche pour oui ou sur 'n' pour non): ").lower()) != "n":       # tant qu'il sa reponse est oui
+    
     print("\n"+"-"*50)
     print("Bienvenue au jeu du: Pierre - Feuille - Ciseaux")
     print("-"*50 +"\n")
- 
-    a = int(input("Choisissez un chiffre:\n0: Pierre\n1: Feuille\n2: Ciseaux\nvotre choix:  "))     #choix de l'utilisateur
-    b = choice(range(3))                                                                            # choix de la machine
-    if a in [0,1,2]:
-        print("\n{} VS {}".format(COUP[a], COUP[b]))               # message: choix de l'utilisateur contre choix de la machine                                
+    try:
+        
+        a = int(input("Choisissez un chiffre:\n 0: Pierre\n 1: Feuille\n 2: Ciseaux\n votre choix:  "))     #choix de l'utilisateur
+        b = choice(range(3))                                                                            # choix de la machine
+        if a in [0,1,2]:
+            
+            nbre_coup += 1                                          # on incremente le nombre de tour joué
 
-        if a == b:
-            print("ÉGALITÉ\n")                                      # si les deux font le meme choix il y'a egalité egalité
-            score_egal += 1
+            print("\n {} VS {}".format(COUP[a], COUP[b]))               # message: choix de l'utilisateur contre choix de la machine                                
 
-        elif (a>b and b+1==a) or (a<b and a+b==2):            # sinon si l'utilisateur choit par exemple 0:Pierre et la machine choisit 2: ciseaux, 0 < 2 et 0+2 = 2 
-            print("VOUS GAGNEZ\n")                            # alors il gagne car la pierre bat le ciseau
-            score += 1                                        # on augmente 1 à score a chaque fois qu'il gagne
+            if a == b:
+                print("ÉGALITÉ\n")                                      # si les deux font le meme choix il y'a egalité egalité
+                score_egal += 1
 
-        else:                                                  # par contre s'il choisit 0:pierre et la machine choisit 1:feuille, 0<1 mais 0+1 != 2
-            print("VOUS PERDEZ\n")                              # alors il perd car la feuille bat la pierre
-    else:
-        print("faites un choix dans [0, 2]")
+            elif (a>b and b+1==a) or (a<b and a+b==2):            # sinon si l'utilisateur choit par exemple 0:Pierre et la machine choisit 2: ciseaux, 0 < 2 et 0+2 = 2 
+                print("VOUS GAGNEZ\n")                            # alors il gagne car la pierre bat le ciseau
+                score += 1                                        # on augmente 1 au score à chaque fois qu'il gagne
+
+            else:                                                  # par contre s'il choisit 0:pierre et la machine choisit 1:feuille, 0<1 mais 0+1 != 2
+                print("VOUS PERDEZ\n")                              # alors il perd car la feuille bat la pierre
+        else:
+            print("choix invalide")                        # si le choix un mauvais numero on ne le penalise pas
+    except ValueError:
+        print("choix invalide")
 else:
     print("\n vous avez gagné  ", score," coups sur ",nbre_coup,"avec ",score_egal,"match null \n merci pour la partie")         # quand il decide de ne plus jouer on lui affiche son resultat
