df36adf5bb3417441d630ced62ee4d0d4884c16f
Chanelle Kapawou <chanellekapawou@gmail.com>
2021-06-10 12:13:23 +0100

    Exexcice 4-1 terminé

--- cuisine_1/pypath_5/Exercice 4-1/Exercice 4-1.py
+++ cuisine_1/pypath_5/Exercice 4-1/Exercice 4-1.py
@@ -1,7 +1,9 @@
 def decorateur(funct):
-    ''' decoateur qui permettra d'encardre un text selon sa mise en forme'''
+    ''' decorateur qui permettra d'encardrer un text selon sa mise en forme'''
     
     def decore(text,g=False,i=False,b=False):
+
+        # en fonction de la mise en forme, on encardre le texte par le symbole correspondant 
                                       
         if g == True :
             text = "*"+text+"*"
@@ -12,20 +14,23 @@ def decorateur(funct):
         if b == True:
              text = "~"+text+"~"
              
-        return funct(text)
+        return funct(text)          
 
                          
     return decore
 
 
-
+# on applique le décorateur à notre fonction
 
 @decorateur
 def style(text, g=False, i=False, b=False):
 
     return text
 
+# test
 print(style("Bonjour", g  = True, i = True, b = True))
+print(style("SEED", i= True, g  = True) )
+print(style("path python", b = True))
 
 
 
